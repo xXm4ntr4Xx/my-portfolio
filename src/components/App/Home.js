7bb085fb4d7c8e9941204abd4476e08ce24ba18b
bladeyugo88 <pietroannobil@gmail.com>
2022-02-07 10:59:47 +0000

    move social link to footer

--- src/components/App/Home.js
+++ src/components/App/Home.js
@@ -14,7 +14,9 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (<>
-     <Social />
+      <Link to="/about"><i class="fas fa-user-tie fa-3x about">About Me</i> </Link>
+      <Link to="/project"><i class="fas fa-briefcase fa-3x project">Project</i></Link>
+     {/* <Social /> */}
       <Header />
        
         <div className='second_box'>
@@ -37,12 +39,8 @@ function Home() {
     
     <div>
 
-      <Link to="/about"><i class="fas fa-user-tie fa-3x about">About</i> </Link>
-      <Link to="/project"><i class="fas fa-briefcase fa-3x project">Project</i></Link>
+       <Social />
     </div>
-      
-    
-        
   </>)
 }
 
