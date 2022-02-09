ce489cae397038d03b457032c532f9a33a23f678
bladeyugo88 <pietroannobil@gmail.com>
2022-02-09 23:25:46 +0000

    redirect

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,7 +12,7 @@ function Home() {
       <a href="/project"><i class="fas fa-briefcase fa-3x project">Project</i></a>
       <a href="/about"><i class="fas fa-user-tie fa-3x about">About Me</i> </a>
      
-       
+    
       <Header />
         
         <div className='second_box'>
