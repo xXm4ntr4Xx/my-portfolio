cf84c9074e47c84de2da9039da3ded1910fcb63b
bladeyugo88 <pietroannobil@gmail.com>
2022-02-11 20:28:58 +0000

    table project

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,9 +15,10 @@ function About() {
     <br />
     <br />
     <p className='text animate__animated animate__zoomInLeft'> 33 years old guy from Italy, and after 10 years working in a casino i decided to join this new adventure in the tech industry</p>
-    <p className='text animate__animated animate__zoomInRight'> During my experience with school of code i learn the basic of <b className='html'>Html</b>, <b className='css'>Css </b> and <b className='js'>Javascript</b></p>
-    <p className='text animate__animated animate__zoomInLeft'> After the first week </p>
-
+    <p className='text animate__animated animate__zoomInRight'> During my experience with school of code i learn the basic of <b className='topic'>Html</b>, <b className='topic'>Css </b> and <b className='topic'>Javascript</b></p>
+    <p className='text animate__animated animate__zoomInLeft'> The importance of testing using tool like <b className='topic'>Jest</b>, <b className='topic'>Cypress</b>, work with databse using <b className='topic'>Node.js</b>and <b className='topic'>Express</b></p>
+    <p className='text animate__animated animate__zoomInRight'> Learn how to CRUD application using api platform like <b className='topic'>Postman</b> and <b className='topic'>Thunder</b></p>
+    
   </div>  
   )
 }
--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -5,8 +5,8 @@ function Social() {
   return (
     <div className='social_bar'>
         <div className='second_layer'>
-             <a href='https://www.linkedin.com/in/pietro-annobil-025483180/'><i class="icons fab fa-linkedin fa-3x"></i></a>
-             <a href='https://github.com/xXm4ntr4Xx'><i class="icons fab fa-github fa-3x"></i></a>
+             <a  className='social-link' href='https://www.linkedin.com/in/pietro-annobil-025483180/'><i class="icons fab fa-linkedin fa-3x"></i></a>
+             <a  className='social-link' href='https://github.com/xXm4ntr4Xx'><i class="icons fab fa-github fa-3x"></i></a>
             
         </div> 
     </div>
