f4597151cd4e3c2849bb0fc6e9efbca0eacd0c0c
bladeyugo88 <pietroannobil@gmail.com>
2022-02-14 19:19:10 +0000

    responsive

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -17,44 +17,21 @@ function Project() {
         <h1 className='animate__animated animate__backInLeft'>City weather API</h1>
 
         <div className='video-description'>
-          
-          <video width="500" height="300"  controls>
-          <source src='image/ezgif.com-gif-maker.gif' type="video/mov"/>
-          </video>
+        <img className='weatherAPI' src='https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/b7cd41d4-884d-451f-b256-0335af874c47.gif?ClientID=vimeo-core-prod&Date=1644864340&Signature=e2e9c7e9f40cdf5d7f13c5ac3996a13d070426ba'/>
           <div>
           <h2>Description<br/><p>First Project of School of Code where i create a City Info using e weather API.
             <br/> The app has been created using Vanilla Javascript,Html and Css. The Most challenge <br/>part of this
              project has been retreive the info from  the API and assign the different data <br/>to a variable and place the info on the page.
              <br/>Was the first time i use  Css in a complete project for create a table and add weather icon on the page.</p></h2>
             <p><bold>Tools : </bold>Html,Css,Vanilla Javascript, FetchAPI</p>
-          </div>
-          
-          
+            <a  href='https://weather-application001.herokuapp.com/ '>Weather Api Link</a>
+          </div> 
+          <br/>
+            
         </div>
-        
-        
-       </div>
 
+        <hr/>
 
-       <div className='table'>
-        <h1 className='animate__animated animate__backInLeft'>City weather API</h1>
-
-        <div className='video-description'>
-          
-          
-          <img src="ezgif.com-gif-maker.gif" alt="This will display an animated GIF" />
-          
-          <div>
-          <h2>Description<br/><p>First Project of School of Code where i create a City Info using e weather API.
-            <br/> The app has been created using Vanilla Javascript,Html and Css. The Most challenge <br/>part of this
-             project has been retreive the info from  the API and assign the different data <br/>to a variable and place the info on the page.
-             <br/>Was the first time i use  Css in a complete project for create a table and add weather icon on the page.</p></h2>
-            <p><bold>Tools : </bold>Html,Css,Vanilla Javascript, FetchAPI</p>
-          </div>
-          
-          
-        </div>
-        
         
        </div>
     </div>  
