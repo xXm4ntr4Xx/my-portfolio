c98c12b9d91676c86f8155562e3f805f7a190823
bladeyugo88 <pietroannobil@gmail.com>
2022-02-14 23:06:26 +0000

    responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,21 @@ import '../styles/home.css';
 import Menu from '../components/Menu/menu';
 
 function Home() {
-  // <Link to='/project' >Project</Link>
-  // <Link to='/about' >About</Link>
+  
+
   return (   
     <div className='home-body'>
       <Menu />
-         <h1 className='header1'>Hi. Im Pietro. <br/> Software developer <br/>based in london</h1>
+         <h1 className='header1'> Pietro Annobil </h1><br/>
+          <p className='par-home'>Im a software developer based in london that love work with API</p>
+          <p className='par-home'>My favourite tools are Html ,Node.js and React</p>
+      <br/>
+
+      <div className='social-link'>
+        <a href=''><h2 className='github'>Github  <i class="fab fa-github-square fa-spin fa-3x"></i></h2></a>
+        <a href=''><h2 className='linkedin'>LinkedIn  <i class="fab fa-linkedin fa-spin fa-3x"></i></h2></a>
+      </div>
+      
     </div>
   )
 }
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -14,39 +14,28 @@ function Project() {
 
 
        <div className='table'>
-        <h1 className='animate__animated animate__backInLeft'>City weather API</h1>
+        <h1 className='header-project animate__animated animate__backInLeft' styles={{color:'white'}}>City weather API</h1>
 
         <div className='video-description'>
-        <img className='weatherAPI' src='https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/b7cd41d4-884d-451f-b256-0335af874c47.gif?ClientID=vimeo-core-prod&Date=1644864340&Signature=e2e9c7e9f40cdf5d7f13c5ac3996a13d070426ba'/>
-          <div className='text-div'>
-          <h2>Description<br/><p>First Project of School of Code where i create a City Info using e weather API.
-            <br/> The app has been created using Vanilla Javascript,Html and Css. The Most challenge <br/>part of this
-             project has been retreive the info from  the API and assign the different data <br/>to a variable and place the info on the page.
-             <br/>Was the first time i use  Css in a complete project for create a table and add weather icon on the page.</p></h2>
-            <p><bold>Tools : </bold>Html,Css,Vanilla Javascript, FetchAPI</p>
-            <a  href='https://weather-application001.herokuapp.com/ '>Weather Api Link</a>
-          </div> 
-          <br/>
-            
-        </div>
-
-       
-
-        <div className='video-description'>
-          <div className='text-div'>
-          <h2>Description<br/><p>First Project of School of Code where i create a City Info using e weather API.
-            <br/> The app has been created using Vanilla Javascript,Html and Css. The Most challenge <br/>part of this
-             project has been retreive the info from  the API and assign the different data <br/>to a variable and place the info on the page.
-             <br/>Was the first time i use  Css in a complete project for create a table and add weather icon on the page.</p></h2>
-            <p><bold>Tools : </bold>Html,Css,Vanilla Javascript, FetchAPI</p>
-            <a  href='https://weather-application001.herokuapp.com/ '>Weather Api Link</a>
-          </div> 
+        <a  href='https://weather-application001.herokuapp.com/ '>
           <img className='weatherAPI' src='https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/b7cd41d4-884d-451f-b256-0335af874c47.gif?ClientID=vimeo-core-prod&Date=1644864340&Signature=e2e9c7e9f40cdf5d7f13c5ac3996a13d070426ba'/>
+        </a>
+        
+          <div className='text-div'>
+          <h2>Description<br/><p>First Project of School of Code where i create a City Info using e weather API.
+            <br/> The app has been created using Vanilla Javascript,Html and Css. The Most challenge <br/>part of this
+             project has been retreive the info from  the API and assign the different data <br/>to a variable and place the info on the page.
+             <br/>Was the first time i use  Css in a complete project for create a table and add weather icon on the page.</p></h2>
+            <p><bold>Tools : </bold>Html,Css,Vanilla Javascript, FetchAPI</p>
+            <a  style={{color:'white'}}href='https://weather-application001.herokuapp.com/ '>Weather Api Link</a>
+          </div> 
           <br/>
             
         </div>
 
-        
+
+          
+
        </div>
     </div>  
   )
