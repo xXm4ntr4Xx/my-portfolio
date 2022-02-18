25750476672c9042223a825d4bd45b58faf7fa7e
bladeyugo88 <pietroannobil@gmail.com>
2022-02-18 00:42:57 +0000

    wip

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React ,{useRef}from 'react';
 import '../styles/contact.css';
 import Menu from '../components/Menu/menu';
+// import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 
 
 function About() {
-
-
-const handleSubmit = () => {
+  const form = useRef({});
   
-}
+const sendEmail = (e) => {
+    e.preventDefault();
+
+//   emailjs.sendForm('service_txg5vy7','template_plwfnyd',form.current,'user_WjBDwlObMa9kxwB0CQf2h')
+//   .then((result) => {
+//     console.log(result.text);
+// }, (error) => {
+//     console.log(error.text);
+// });
+
+//   e.target.reset();
+  }
+
 
   return (  
       <div className='contact-container'>
@@ -18,16 +30,29 @@ const handleSubmit = () => {
         
         <div className='social-list'>
             <p><span>Telephone : </span>07501971950</p>
-            <p>Youtube : <a href='https://www.youtube.com/channel/UC5vMCzzUNOSlrxbphd4k7SA'><i class="fab fa-youtube"></i></a></p>
-            <p>Instagram : <a href='https://www.instagram.com/xxm4ntr4xx/'><i class="fab fa-instagram"></i></a></p>
-            <p> Faceook : <a href='https://www.facebook.com/pietro.annobil'><i class="fab fa-facebook-square"></i></a></p>
-            <p>Twitter : <a href='https://twitter.com/AnnobilPietro'><i class="fab fa-twitter"></i></a></p>
-            <p>Pinterest : <a href='https://www.pinterest.co.uk/'><i class="fab fa-pinterest"></i></a></p>
+            <p>Youtube : <a href='https://www.youtube.com/channel/UC5vMCzzUNOSlrxbphd4k7SA'><i className="fab fa-youtube"></i></a></p>
+            <p>Instagram : <a href='https://www.instagram.com/xxm4ntr4xx/'><i className="fab fa-instagram"></i></a></p>
+            <p> Faceook : <a href='https://www.facebook.com/pietro.annobil'><i className="fab fa-facebook-square"></i></a></p>
+            <p>Twitter : <a href='https://twitter.com/AnnobilPietro'><i className="fab fa-twitter"></i></a></p>
+            <p>Pinterest : <a href='https://www.pinterest.co.uk/'><i className="fab fa-pinterest"></i></a></p>
         </div>
+
+      <br/>
+
+      
+      {/* <form action='POST'ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form> */}
           
 </div>
      
   )
 }
 
-export default About
+export default About;
