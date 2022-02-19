96f281657e8ab82072c70bb7bc1d368649ab5ee7
bladeyugo88 <pietroannobil@gmail.com>
2022-02-19 01:01:49 +0000

    up

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -19,7 +19,7 @@ function About() {
             <p>Linkedin : <a href='https://www.linkedin.com/in/pietro-annobil-025483180/'><i className="fab fa-linkedin"></i></a></p>
             <p>Twitter : <a href='https://twitter.com/AnnobilPietro'><i className="fab fa-twitter"></i></a></p>
             <p>Pinterest : <a href='https://www.pinterest.co.uk/'><i className="fab fa-pinterest"></i></a></p>
-            <p> Faceook : <a href='https://www.facebook.com/pietro.annobil'><i className="fab fa-facebook-square"></i></a></p>
+            <p> Facebook : <a href='https://www.facebook.com/pietro.annobil'><i className="fab fa-facebook-square"></i></a></p>
             <p>Youtube : <a href='https://www.youtube.com/channel/UC5vMCzzUNOSlrxbphd4k7SA'><i className="fab fa-youtube"></i></a></p>
             <p>Instagram : <a href='https://www.instagram.com/xxm4ntr4xx/'><i className="fab fa-instagram"></i></a></p>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,9 +14,8 @@ function Home() {
       <br/>
 
       <div className='social-link'>
-        <a href=''><h2 className='github'>Github  <i class="fab fa-github-square fa-spin fa-3x"></i></h2></a>
-        <a href=''><h2 className='linkedin'>LinkedIn  <i class="fab fa-linkedin fa-spin fa-3x"></i></h2></a>
-        
+        <a href='https://github.com/xXm4ntr4Xx'><h2 className='github'>Github  <i class="fab fa-github-square fa-spin fa-3x"></i></h2></a>
+        <a href='https://www.linkedin.com/in/pietro-annobil-025483180/'><h2 className='linkedin'>LinkedIn  <i class="fab fa-linkedin fa-spin fa-3x"></i></h2></a>
       </div>
       
     </div>
